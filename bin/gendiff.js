d3b009a1ba292cd96eaba08fb186fabbfd105498
gedo19 <eu.MaD19@yandex.ru>
2021-12-07 19:08:19 +0600

    add gendiff.js, add programm description and help via commander.js

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+program.parse();
