95eb108d8391adbba1256d1d5387eb087a99bc92
gedo19 <eu.MaD19@yandex.ru>
2021-12-07 19:19:31 +0600

    add arguments and [format] option to cli program

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
