fb327c30f64e188e16bde78e4e9e90959a92563c
gedo19 <eu.MaD19@yandex.ru>
2021-12-08 20:29:08 +0600

    linter fixes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,15 @@
 import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { test, expect, beforeAll } from '@jest/globals';
 import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const readFile = (filename) =>
-  fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 let filepath1;
 let filepath2;
