512a3ee98a5d55be67771dbfdac338de9810c5c2
gedo19 <eu.MaD19@yandex.ru>
2021-12-10 20:03:30 +0600

    separate parser

--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
 import * as fs from 'fs';
-import _ from 'lodash';
+import path from 'path';
+import compareObjects from './compareObjects.js';
+import parse from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
-  const fileContent1 = fs.readFileSync(filepath1, 'utf-8');
-  const fileContent2 = fs.readFileSync(filepath2, 'utf-8');
-  const file1 = JSON.parse(fileContent1);
-  const file2 = JSON.parse(fileContent2);
-  const keys = _.union(Object.keys(file1), Object.keys(file2));
-  const sortedKeys = _.sortBy(keys);
+  const data1 = fs.readFileSync(filepath1, 'utf-8');
+  const data2 = fs.readFileSync(filepath2, 'utf-8');
+  const format1 = path.extname(filepath1);
+  const format2 = path.extname(filepath2);
+  const object1 = parse(data1, format1);
+  const object2 = parse(data2, format2);
 
-  const difference = sortedKeys.reduce((diff, key) => {
-    if (!_.has(file1, key)) {
-      const value = file2[key];
-      return `${diff}  + ${key}: ${value}\n`;
-    }
-    if (!_.has(file2, key)) {
-      const value = file1[key];
-      return `${diff}  - ${key}: ${value}\n`;
-    }
-
-    const value1 = file1[key];
-    const value2 = file2[key];
-
-    return value1 === value2
-      ? `${diff}    ${key}: ${value2}\n`
-      : `${diff}  - ${key}: ${value1}\n  + ${key}: ${value2}\n`;
-  }, '');
-
-  return `{\n${difference}}`;
+  return compareObjects(object1, object2);
 };
 
 export default genDiff;
