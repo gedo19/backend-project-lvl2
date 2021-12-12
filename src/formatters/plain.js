9bf2b341077fe33df37ab0ba21d1b768741a6e89
gedo19 <eu.MaD19@yandex.ru>
2021-12-12 23:29:52 +0600

    small refractoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,44 +7,40 @@ const normalize = (value) => {
   if (_.isObject(value)) {
     return '[complex value]';
   }
-  if (_.isString(value)) {
-    return `'${value}'`;
-  }
-  if (value === '') {
-    return '\'\'';
-  }
 
-  return value;
+  return _.isString(value) ? `'${value}'` : value;
 };
 
 const plain = (differenceTree) => {
-  const iter = (properties, line) => {
-    const lines = properties.flatMap((property) => {
-      const key = getKey(property);
+  const iter = (properties, path) => {
+    const lines = properties
+    .map((property) => {
+      const keys = [...path, getKey(property)];
       const value = getValue(property);
       const status = getStatus(property);
       if (isFlatProperty(property)) {
+        const propertyName = keys.join('.')
         const normalizedValue = normalize(value);
         switch (status) {
           case 'added':
-            return `${line}${key}' was added with value: ${normalizedValue}`;
+            return `Property '${propertyName}' was added with value: ${normalizedValue}`;
           case 'deleted':
-            return `${line}${key}' was removed`;
+            return `Property '${propertyName}' was removed`;
           case 'changed': {
             const normalizedOldValue = normalize(getOldValue(property));
-            return `${line}${key}' was updated. From ${normalizedOldValue} to ${normalizedValue}`;
+            return `Property '${propertyName}' was updated. From ${normalizedOldValue} to ${normalizedValue}`;
           }
           default:
             return '';
         }
       }
 
-      return iter(value, `${line}${key}.`);
+      return iter(value, keys);
     });
     return lines.filter((e) => e).join('\n');
   };
 
-  return iter(differenceTree, 'Property \'');
+  return iter(differenceTree, []);
 };
 
 export default plain;
