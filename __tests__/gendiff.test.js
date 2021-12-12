09c8e98244ff58c8097b0edc278a68a7d97f6612
gedo19 <eu.MaD19@yandex.ru>
2021-12-12 16:34:54 +0600

    remove plain tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,17 +13,11 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 let nestedStylishFormat;
 let nestedPlainFormat;
-let parsedData;
+
 
 beforeAll(() => {
   const data = readFile('nested_result.txt');
   [nestedStylishFormat, nestedPlainFormat] = data.trim().split('\n\n\n');
-  parsedData = {
-    host: 'hexlet.io',
-    timeout: 50,
-    proxy: '123.234.53.22',
-    follow: false,
-  };
 });
 
 test('nested stylish-formatter json shoud work', () => {
@@ -44,18 +38,4 @@ test('nested plain-formatter json shoud work', () => {
 test('nested plain-formatter yaml shoud work', () => {
   const expected = getDiff(getFixturePath('nested1.yaml'), getFixturePath('nested2.yaml'), 'plain');
   expect(expected).toEqual(nestedPlainFormat);
-});
-
-test('parser shour parse json files format', () => {
-  const expected = parse(readFile('plain1.json'), '.json');
-  expect(expected).toEqual(parsedData);
-});
-
-test('parser shour parse yaml files format', () => {
-  const expected = parse(readFile('plain1.yaml'), '.yaml');
-  expect(expected).toEqual(parsedData);
-});
-
-test('parser shour throw error when recieve not supported file format', () => {
-  expect(() => parse(readFile('file1.yaml'), '')).toThrow();
 });
